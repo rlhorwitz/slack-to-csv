377afa8313454d3de3e2971a968b794389e315bc
Rachel Horwitz <rachel@Rachels-MBP.lan>
2020-11-28 18:07:29 -0500

    made improvements to readability

--- script.js
+++ script.js
@@ -4,34 +4,34 @@ const fs = require("fs-extra");
 
 const DATA_PATH = './data';
 
-const jsonToCSV = (rootDir, parentDir, filename) => {
-    fs.readFile(`${rootDir}/${parentDir}/${filename}`, (readFileErr, data) => {
-      let csvString = "User,Timestamp,Message Text,Attachment(s)\n";
-      const messages = JSON.parse(data);
-      messages.forEach(message => {
-          const user = users.find(user => user.id == message.user);
-        csvString += `"${user && user.real_name ? user.real_name : "Unknown User"}","${luxon.DateTime.fromSeconds(parseInt(message.ts)).toRFC2822()}","${message.text}"`;
-        if(message.attachements && message.attachments.length > 0) {
-          csvString += `"${message.attachments.map(attachment => attachment.original_url).join(',')}"`;
-        }
-        csvString += '\n';
+const jsonToCSV = async (rootDir, parentDir, filename) => {
+  const data = await fs.readFile(`${rootDir}/${parentDir}/${filename}`);
+  let csvString = "User,Timestamp,Message Text,Attachment(s)\n";
+  const messages = JSON.parse(data);
+  messages.forEach(message => {
+    const user = users.find(user => user.id == message.user);
+    csvString += `"${user && user.real_name ? user.real_name : "Unknown User"}","${luxon.DateTime.fromSeconds(parseInt(message.ts)).toRFC2822()}","${message.text}"`;
+    if (message.attachments && message.attachments.length > 0) {
+      csvString += `"${message.attachments.map(attachment => attachment.original_url).join(',')}"`;
+    }
+    csvString += '\n';
 
-        const outputFileName = `./output/${parentDir}/${filename.split('.')[0]}.csv`
-        fs.outputFile(outputFileName, csvString);
-      });
-    });
+    const outputFileName = `./output/${parentDir}/${filename.split('.')[0]}.csv`
+    fs.outputFile(outputFileName, csvString);
+  });
 };
 
-fs.readdir(DATA_PATH, (dataDirReadErr, items) => {
-    items.forEach(item => {
-        fs.stat(`${DATA_PATH}/${item}`, (dataDirStatErr, stats) => {
-            if (stats.isDirectory()) {
-                fs.readdir(`${DATA_PATH}/${item}`, (fileDirReadErr, jsonFiles) => {
-                    jsonFiles.forEach(jsonFile => {
-                      jsonToCSV(DATA_PATH, item, jsonFile);
-                    });
-                });
-            }
-        });
-    });
-});
+const main = async () => {
+  const dataDirContents = await fs.readdir(DATA_PATH);
+  dataDirContents.forEach(async (item) => {
+    const stats = await fs.stat(`${DATA_PATH}/${item}`);
+    if (stats.isDirectory()) {
+      const jsonFiles = await fs.readdir(`${DATA_PATH}/${item}`);
+      jsonFiles.forEach(jsonFile => {
+        jsonToCSV(DATA_PATH, item, jsonFile);
+      });
+    }
+  });
+}
+
+main();
